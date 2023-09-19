50646ef14af5a0a14ac0593fb5909f94c77fe25e
kweons01 <soyoun.kweon@mssm.edu>
2023-09-19 18:01:52 -0400

    updated director

--- system-huddle-render.R
+++ system-huddle-render.R
@@ -1,7 +1,9 @@
 library(rmarkdown)
 library(conflicted)
 
+
 rmarkdown::render(input = "system-huddle-all.Rmd",
                   output_format = "html_document",
                   output_file = "system_huddle_test",
-                  output_dir = "/nfs/data/Applications/Ambulatory/")
+                  output_dir = "/nfs/data/Applications/Ambulatory",
+                  params = list(output_dir = "/nfs/data/Applications/Ambulatory"))
